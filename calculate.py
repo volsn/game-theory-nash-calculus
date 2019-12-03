76001d456671818a6f6afa3da32e531352872176
Nikolaj Volosnikoff <volosnikovkolia@gmail.com>
2019-12-03 20:49:59 +0200

    Completed.

--- calculate.py
+++ calculate.py
@@ -16,7 +16,21 @@ p0 = solve_poly_inequality(Poly(-(C * x - a)), '>=')[0]
 q1 = solve_poly_inequality(Poly(D * x - b), '>=')[0]
 q0 = solve_poly_inequality(Poly(-(D * x - b)), '>=')[0]
 
+p = Intersection(q1, q0).sup
+q = Intersection(p1, p0).sup
+
+Ha = (a22 / ((1-p) * (1-q))) + (a21 / (q * (1-p))) + (a12 / (p * (1-q))) + (a11 / (p*q))
+Hb = (b22 / ((1-p) * (1-q))) + (b21 / (q * (1-p))) + (b12 / (p * (1-q))) + (b11 / (p*q))
+
 with open('output.txt', 'w') as f:
     f.write("Оптимальные стратегии по Нэшу:\n")
     f.write("P* = ({}; {})\n".format(Intersection(q1, q0).sup, abs(Intersection(q1, q0).sup - 1)))
-    f.write("Q* = ({}; {})\n".format(Intersection(p1, p0).sup, abs(Intersection(p1, p0).sup - 1)))
+    f.write("Q* = ({}; {})\n\n".format(Intersection(p1, p0).sup, abs(Intersection(p1, p0).sup - 1)))
+    
+    f.write("Цена игры для первого игрока:\n")
+    f.write("Ha(p;q) = {}\n".format(Ha))
+    f.write("Цена игры для второго игрока:\n")
+    f.write("Hb(p;q) = {}\n\n".format(Hb))
+    
+    f.write("Выигрыш игроков в равновесной ситуации:\n")
+    f.write("f(P*,Q*) = ({}; {})\n".format(Ha, Hb))
